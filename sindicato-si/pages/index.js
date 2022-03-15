4e82765bb66a7ca3c519192e53b24b23bc5fcaf1
EduardoAyora <eduardoaayora64@gmail.com>
2022-03-14 23:53:50 -0500

    Agregar inscripciones tipo E

--- sindicato-si/pages/index.js
+++ sindicato-si/pages/index.js
@@ -16,6 +16,8 @@ export default function Home() {
 
   const cNameRef = useRef()
   const cCelRef = useRef()
+  const eNameRef = useRef()
+  const eCelRef = useRef()
 
   const toBase64 = (file) =>
     new Promise((resolve, reject) => {
@@ -306,17 +308,195 @@ export default function Home() {
                             SecureToken: '9ad5ea20-233e-400d-835d-d5df0e7a8486',
                             To: 'eduardoaayora24@gmail.com',
                             From: 'eduardoaayora24@gmail.com',
-                            Subject: 'SOLICITUD DE INSCRIPCIÓN',
-                            Body: `Nombre: ${cNameRef.current.value}, Celular: ${cCelRef.current.value}`,
+                            Subject:
+                              'SOLICITUD DE INSCRIPCIÓN, Licencia tipo C',
+                            Body: `Licencia tipo C, \n Nombre: ${cNameRef.current.value}, Celular: ${cCelRef.current.value}`,
                             Attachments: mappedPdfsToSend,
                           })
                             .then((message) => {
                               alert(
-                                'Sus datos se han enviado con éxito: ' + message
+                                'Los datos para su inscripción se han enviado con éxito: ' +
+                                  message
                               )
                               setCFiles([])
                               cNameRef.current.value = ''
                               cCelRef.current.value = ''
+                            })
+                            .catch((err) => {
+                              alert('Ha ocurrido el error: ' + err)
+                              setCSendEnabled(true)
+                            })
+                        }}
+                        disabled={!cSendEnabled}
+                        className={`${
+                          cSendEnabled
+                            ? 'bg-green-600 hover:bg-green-700 focus:ring-green-500'
+                            : 'bg-gray-600 hover:bg-gray-700 focus:ring-gray-500'
+                        } inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white  focus:outline-none focus:ring-2 focus:ring-offset-2`}
+                      >
+                        Enviar
+                      </button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+
+            <div className='pt-12 sm:mt-0 text-left'>
+              <div className='mt-5 md:mt-0 md:col-span-2'>
+                <form action='#' method='POST'>
+                  <div className='shadow overflow-hidden sm:rounded-md'>
+                    <div className='px-4 py-5 bg-white sm:p-6'>
+                      <h2 className='text-2xl font-semibold text-gray-800'>
+                        Inscripciones para licencia tipo E
+                      </h2>
+                      <div className='grid grid-cols-6 gap-6 pt-4'>
+                        <div className='col-span-6 sm:col-span-3'>
+                          <label className='block text-sm font-medium text-gray-700'>
+                            Nombre
+                          </label>
+                          <input
+                            ref={eNameRef}
+                            type='text'
+                            className='mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md'
+                          />
+                        </div>
+
+                        <div className='col-span-6 sm:col-span-3'>
+                          <label className='block text-sm font-medium text-gray-700'>
+                            Celular
+                          </label>
+                          <input
+                            ref={eCelRef}
+                            type='text'
+                            className='mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md'
+                          />
+                        </div>
+
+                        <div className='col-span-6 sm:col-span-6'>
+                          <label className='block text-sm font-medium text-gray-700'>
+                            <p className='font-bold'>
+                              Subir los siguientes documentos:
+                            </p>
+                            <ul className='list-disc pl-4'>
+                              <li>
+                                Cédula de identidad, certificado de votación,
+                                carnet de tipo de sangre (Cruz Roja). (En .pdf
+                                .doc o .docx)
+                              </li>
+                              <li>
+                                Certificado de estudios, aprobado el primer año
+                                de bachillerato. (Sellado por el distrito de
+                                educación)
+                              </li>
+                              <li>
+                                Licencia tipo C (misma que debe estar vigente,
+                                no tener multas y tener puntos)
+                              </li>
+                              <li>
+                                Record y contenido académico de la escuela donde
+                                hizo el curso tipo C
+                              </li>
+                              <li>
+                                Certificado de conductor emitido por la ANT.
+                              </li>
+                              <li>
+                                Original del carnet de tipo de sangre (Emitido
+                                por cruz roja) -
+                              </li>
+                              <li>Exámenes pscicosensométricos -</li>
+                              <li>Certificado de salud (Emitido por MSP)</li>
+                            </ul>
+                          </label>
+                          <div className='mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md'>
+                            <div className='space-y-1 text-center'>
+                              <div className='flex justify-center text-sm text-gray-600'>
+                                <label>
+                                  <input
+                                    accept='.pdf, .doc, .docx'
+                                    type='file'
+                                    multiple
+                                    onChange={(e) => {
+                                      var files = e.target.files
+                                      var filesArr =
+                                        Array.prototype.slice.call(files)
+                                      setEFiles((prevFiles) => {
+                                        return [...prevFiles, ...filesArr]
+                                      })
+                                    }}
+                                  />
+                                  <div className='cursor-pointer w-full bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'>
+                                    <CloudUploadIcon
+                                      className='h-6 w-6 mr-2'
+                                      aria-hidden='true'
+                                    />{' '}
+                                    Cargar documentos
+                                  </div>
+                                </label>
+                              </div>
+                              <p className='text-xs text-gray-500'>
+                                PDF, DOC, DOCX
+                              </p>
+
+                              <div style={styles}>
+                                {eFiles.map((file, indice) => (
+                                  <span
+                                    onClick={() =>
+                                      setEFiles(
+                                        eFiles.filter((x) => x !== file)
+                                      )
+                                    }
+                                    key={indice}
+                                    className='m-1 bg-gray-200 hover:bg-gray-300 rounded-full px-2 font-bold text-sm leading-loose cursor-pointer'
+                                  >
+                                    {file.name}
+                                  </span>
+                                ))}
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className='px-4 py-3 bg-gray-50 text-right sm:px-6'>
+                      <button
+                        type='button'
+                        onClick={async () => {
+                          if (eNameRef.current.value === '')
+                            return alert('El nombre es requerido')
+                          if (eCelRef.current.value === '')
+                            return alert('El celular es requerido')
+                          if (eFiles.length === 0)
+                            return alert('Cargar los documentos es requerido')
+
+                          const mappedPdfsToSend = []
+                          for await (const file of eFiles) {
+                            const name = file.name
+                            const data = await toBase64(file)
+                            mappedPdfsToSend.push({
+                              name,
+                              data,
+                            })
+                          }
+                          setCSendEnabled(false)
+
+                          Email.send({
+                            SecureToken: '9ad5ea20-233e-400d-835d-d5df0e7a8486',
+                            To: 'eduardoaayora24@gmail.com',
+                            From: 'eduardoaayora24@gmail.com',
+                            Subject:
+                              'SOLICITUD DE INSCRIPCIÓN, Licencia tipo E',
+                            Body: `Licencia tipo E, \n Nombre: ${eNameRef.current.value}, Celular: ${eCelRef.current.value}`,
+                            Attachments: mappedPdfsToSend,
+                          })
+                            .then((message) => {
+                              alert(
+                                'Los datos para su inscripción se han enviado con éxito: ' +
+                                  message
+                              )
+                              setEFiles([])
+                              eNameRef.current.value = ''
+                              eCelRef.current.value = ''
                             })
                             .catch((err) => {
                               alert('Ha ocurrido el error: ' + err)
